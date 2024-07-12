a5e6f475576589098d4dd64f838dfbd0058c95b0
jaychang99 <jaychang99@gmail.com>
2024-07-12 21:44:41 +0900

    feat: setup development environment

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { flattenOpenapi } from './modules/flatten.openapi';
 import { DiffOptions } from './types/diff.options.type';
 import { DiffOutputItem } from './types/diff.output.type';
 import { SpecType } from './types/spec.type';
+import fs from 'fs';
 
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+const EXAMPLE_FILE_BASE_URL = 'src/__local__/examples';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 export function openapiDiff(
   oldSpec: SpecType,
@@ -14,6 +19,16 @@ export function openapiDiff(
 ): DiffOutputItem[] {
   const flattenedOldSpec = flattenOpenapi(oldSpec);
   const flattenedNewSpec = flattenOpenapi(newSpec);
-
   return [];
+}
+
+if (isDevelopment) {
+  function loadSpec(filePath: string) {
+    return JSON.parse(fs.readFileSync(filePath).toString());
+  }
+  const oldSpec = loadSpec(`${EXAMPLE_FILE_BASE_URL}/openapi-old.json`);
+  const newSpec = loadSpec(`${EXAMPLE_FILE_BASE_URL}/openapi-new.json`);
+
+  const diff = openapiDiff(oldSpec, newSpec);
+  console.table(diff);
 }
